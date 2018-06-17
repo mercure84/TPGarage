9a21633894a3f95a8e19a600ff0b345a367f21a8
Mjuju84 <julien.marcesse@gmail.com>
2018-06-17 16:04:28 +0200

    reprise ! impossible de lire le fichier du garage

--- src/garage/Garage.java
+++ src/garage/Garage.java
@@ -1,71 +1,89 @@
 package garage;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import options.Option;
 import voitures.Vehicule;
 
-public class Garage {
+public class Garage implements Serializable {
 
 	List<Vehicule> voitures = new ArrayList();;
 
 	public Garage() {
 
 	}
-/**
- * affiche le garage
- */
+
+	/**
+	 * affiche le garage
+	 */
 	public String toString() {
 
-		Iterator<Vehicule> it = voitures.iterator();
-		String description = "";
+		System.out.println("*****************************");
+		System.out.println("*   Garage OpenClassrooms   *");
+		System.out.println("*****************************");
+		try {
+		ObjectInputStream ois;
+		ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(new File("sauvegarde/save.txt"))));
 
-		while (it.hasNext()) {
+			System.out.println(ois.readObject());
 
-			Vehicule v = it.next();
-			description += v.toString() + "\n";
+			System.out.println(ois.readObject());
 
+
+		
+		
+		
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+			
 		}
+		
 
-		return description;
+		return "";
 
 	}
-/**
- * ajoute une voiture dans le garage et la sauvegarde dans le fichier save.txt
- * @param voiture
- */
+
+	/**
+	 * ajoute une voiture dans le garage et la sauvegarde dans le fichier save.txt
+	 * 
+	 * @param voiture
+	 */
 	public void addVoiture(Vehicule voiture) {
 
 		voitures.add(voiture);
-		
-		// ajout des véhicules dans le ficher save.txt
+
+		// enregistrement du garage dans le fichier(on enregistre l'ArrayList voitures)
 		ObjectOutputStream oos;
 		try {
 			oos = new ObjectOutputStream(
-					new BufferedOutputStream(
-							new FileOutputStream(
-									new File("sauvegarde/save.txt"))));
-			
-		oos.writeObject(voiture);
-		
-		oos.close();
-					
-		}catch (FileNotFoundException e) {
-		      e.printStackTrace();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }  
+					new BufferedOutputStream(new FileOutputStream(new File("sauvegarde/save.txt"), true)));
+
+			oos.writeObject(voiture);
+
+			oos.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
-
 
 }
